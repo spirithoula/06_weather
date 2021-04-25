a89f2d19d1749fc484f219fad5c7a9e7fd3b467e
Alex Harrington <spirithoula2@gmail.com>
2021-04-24 21:57:23 -0500

    weather api

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,14 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={d1635fbaa122518ba37230c95af37abc}')
-    .then (response)
+function searchCity(cityname) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?="+ cityname + "&appid=d1635fbaa122518ba37230c95af37abc"
+
+
+    $.ajax({
+    url: queryURL,
+    method: 'GET'
+    }).then(function (response) {
+    console.log(response);
+    console.log(queryURL);
+    $("#current").empty();
+   var mainDate = moment().format('L');
+    })
+}
